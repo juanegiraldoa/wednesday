c0fec1f9677e854ac35f57b9ba8c80f0797278c8
Juan Giraldo <juanegiraldoa@proton.me>
2024-04-14 19:19:25 -0500

    Add crons

--- index.mjs
+++
@@ -1,8 +0,0 @@
-import env from "dotenv";
-import { postWednesday } from "./src/itswednesday.mjs";
-
-env.config();
-
-export const handler = async (event) => {
-  return { statusCode: 200, body: await postWednesday() };
-};
--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,9 @@ export default {
 		const media_ids: string = env.MASTODON_WEDNESDAY_DUDE_MEDIA;
 		return new Response(await postOnWednesday(token, media_ids));
 	},
+	async scheduled(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const token: string = env.MASTODON_TOKEN;
+		const media_ids: string = env.MASTODON_WEDNESDAY_DUDE_MEDIA;
+		return new Response(await postOnWednesday(token, media_ids));
+	},
 };
