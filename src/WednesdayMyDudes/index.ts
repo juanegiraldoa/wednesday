d3c650cf65754036aa378521fbfcdce8970ff58a
Juan Giraldo <juanegiraldoa@proton.me>
2024-04-17 14:02:59 -0500

    Update vars

--- src/WednesdayMyDudes/index.ts
+++ src/WednesdayMyDudes/index.ts
@@ -6,8 +6,8 @@ const DATE_FORMAT = 'YYYY-MM-DD';
 const postOnWednesday = async (token: string, media_ids: string) => {
 	if (moment().day() === 3) {
 		const [lastPost]: any = await getTimeLine(token);
-		const lastPostDate = moment(lastPost.created_at).format(DATE_FORMAT);
-		if (lastPostDate !== moment().format(DATE_FORMAT)) {
+		const lastPostDate = lastPost ? moment(lastPost.created_at).format(DATE_FORMAT) : null;
+		if (!lastPostDate && lastPostDate !== moment().format(DATE_FORMAT)) {
 			await postStatus(token, media_ids);
 			return "It's Wednesday My Dudes";
 		} else return 'Already Posted My Dude';
