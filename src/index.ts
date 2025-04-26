fc3b77febb7eb9b4aa41d34d957bf37fad5e24fc
Juan Giraldo <juanegiraldoa@proton.me>
2025-04-26 13:28:56 -0500

    Update biome.json

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import { postOnWednesday } from './WednesdayMyDudes';
 
 export interface Env {
-	MASTODON_TOKEN: string;
-	RESOURCES: R2Bucket;
-	BSKY_IDENTIFIER: string;
-	BSKY_PASSWORD: string;
+  MASTODON_TOKEN: string;
+  RESOURCES: R2Bucket;
+  BSKY_IDENTIFIER: string;
+  BSKY_PASSWORD: string;
 }
 
 export default {
-	// async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-	// 	return new Response(await postOnWednesday(env));
-	// },
-	async scheduled(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response(await postOnWednesday(env));
-	},
+  // async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  // 	return new Response(await postOnWednesday(env));
+  // },
+  async scheduled(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+    return new Response(await postOnWednesday(env));
+  },
 };
